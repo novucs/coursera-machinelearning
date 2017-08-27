4ea45b1f5099d6762fb31cf3404e226e0fbc4054
novucs <contact@novucs.net>
2017-08-27 01:29:29 +0100

    [ml_ex5] Compute gradient in linear regression with regularization

--- machinelearning/machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machinelearning/machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,13 +19,13 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+a2 = (X * theta);
+J = (1/(2*m)) * (sum((a2 - y).^2) + lambda * sum(theta(2:end)));
 
-J = (1/(2*m)) * (sum(((X * theta) - y).^2) + lambda * sum(theta(2:end)));
+grad(1) = sum((a2 - y) .* X(:,1));
+grad(2) = sum((a2 - y) .* X(:,2)) + lambda * sum(theta(2:end));
 
-
-
-
-
+grad = (1/m) * grad;
 
 % =========================================================================
 
